faa7cf8f7346e6e302151eb9b0046d07289dabeb
avmaktslave <shlyahovdmitry@gmail.com>
2018-08-03 20:05:36 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './AppStyles';
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+  </View>
+);
+
+export default App;
